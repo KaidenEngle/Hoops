6a4ce7add0fcbc3b6f8c14318a0e352eca9145ca
Kai'den Engle <kaidenengle8@gmail.com>
2024-10-22 22:35:50 -0400

    Working collision detection with player and ball

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ import (
 type Sprites struct {
 	PlayerImage      *ebiten.Image
 	PlayerX, PlayerY float64
+	PlayerL, PlayerH float64
+	isFollowing      bool
 }
 
 type Court struct {
@@ -20,7 +22,7 @@ type Court struct {
 }
 
 type Game struct {
-	Player  *Sprites
+	sprite  *Sprites
 	sprites []*Sprites
 	Court   *Court
 }
@@ -31,31 +33,31 @@ func init() {
 
 func (g *Game) Update() error {
 	if ebiten.IsKeyPressed(ebiten.KeyD) {
-		g.Player.PlayerX += 3
+		g.sprite.PlayerX += 3
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyA) {
-		g.Player.PlayerX -= 3
+		g.sprite.PlayerX -= 3
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyW) {
-		g.Player.PlayerY -= 3
+		g.sprite.PlayerY -= 3
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyS) {
-		g.Player.PlayerY += 3
+		g.sprite.PlayerY += 3
 	}
 
-	for _, ball := range g.sprites {
-		//Ball follows player on collision
-		if g.Player.PlayerX > ball.PlayerX {
-			ball.PlayerX += 2
-		} else if g.Player.PlayerX < ball.PlayerX {
-			ball.PlayerX -= 2
-		}
-		if g.Player.PlayerY > ball.PlayerY {
-			ball.PlayerY += 2
-		} else if g.Player.PlayerY < ball.PlayerY {
-			ball.PlayerY -= 2
-		}
+	ball := g.sprites[1]
 
+	//Ball follows player on collision
+	if ball.PlayerX < g.sprite.PlayerX+g.sprite.PlayerL &&
+		ball.PlayerX+ball.PlayerL > g.sprite.PlayerX &&
+		ball.PlayerY < g.sprite.PlayerY+g.sprite.PlayerH &&
+		ball.PlayerY+ball.PlayerH > g.sprite.PlayerY {
+		ball.isFollowing = true
+	}
+
+	if ball.isFollowing {
+		ball.PlayerX = g.sprite.PlayerX + 20
+		ball.PlayerY = g.sprite.PlayerY + 50
 	}
 
 	return nil
@@ -66,21 +68,17 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	opts := ebiten.DrawImageOptions{}
 	courtOpts := ebiten.DrawImageOptions{}
 	courtOpts.GeoM.Translate(g.Court.CourtX, g.Court.CourtY)
-	opts.GeoM.Translate(g.Player.PlayerX, g.Player.PlayerY)
+	opts.GeoM.Translate(g.sprite.PlayerX, g.sprite.PlayerY)
 
 	screen.DrawImage(g.Court.CourtImage.SubImage(image.Rect(0, 0, 1000, 1000)).(*ebiten.Image),
 		&courtOpts,
-	)
-
-	screen.DrawImage(g.Player.PlayerImage.SubImage(image.Rect(5, 0, 35, 300)).(*ebiten.Image),
-		&opts,
 	)
 
 	opts.GeoM.Reset()
 
 	for _, sprite := range g.sprites {
 		opts.GeoM.Translate(sprite.PlayerX, sprite.PlayerY)
-		screen.DrawImage(sprite.PlayerImage.SubImage(image.Rect(50, 50, 65, 65)).(*ebiten.Image),
+		screen.DrawImage(sprite.PlayerImage.SubImage(image.Rect(0, 0, int(sprite.PlayerL), int(sprite.PlayerH))).(*ebiten.Image),
 			&opts,
 		)
 		opts.GeoM.Reset()
@@ -111,17 +109,22 @@ func main() {
 	}
 
 	game := Game{
-		Player: &Sprites{
-			PlayerImage: PlayerImg,
-			PlayerX:     75,
-			PlayerY:     60,
-		},
 		sprites: []*Sprites{
+			{
+				//Player
+				PlayerImage: PlayerImg,
+				PlayerX:     75,
+				PlayerY:     60,
+				PlayerL:     35,
+				PlayerH:     300,
+			},
 			{
 				//Basketball
 				PlayerImage: BallImg,
 				PlayerX:     115,
 				PlayerY:     80,
+				PlayerL:     10,
+				PlayerH:     10,
 			},
 		},
 		Court: &Court{
@@ -130,6 +133,8 @@ func main() {
 			CourtY:     0,
 		},
 	}
+
+	game.sprite = game.sprites[0]
 
 	if err := ebiten.RunGame(&game); err != nil {
 		log.Fatal(err)
